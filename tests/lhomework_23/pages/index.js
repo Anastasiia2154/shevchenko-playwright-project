abecb1bfc7e69952086f5c87560236e0e3c47ee7
anastasiiamacbook <anastasia.shevchenko2154@gmail.com>
2025-05-18 17:17:58 +0300

    implemented Playwright Network & API Request homework

--- tests/lhomework_23/pages/index.js
+++ tests/lhomework_23/pages/index.js
@@ -1,3 +1,4 @@
 export { default as RegistrationPopUpPage } from './RegistrationPopUpPage.js';
 export { default as GaragePage } from './GaragePage.js';
 export { default as LoginPopUpPage } from './LoginPopUpPage.js';
+export { default as ProfilePage } from './ProfilePage.js';
